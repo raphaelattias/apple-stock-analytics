102d4ede3472c782a09b4bd10376ffbd67aa347e
Camille Frayssinhes <camille.frayssinhes@epfl.ch>
2021-11-30 15:36:13 +0100

    task3

--- task3.py
+++ task3.py
@@ -1,2 +1,13 @@
-def task3():
+def task3(quotes):
+    
+    def sentiment(quote) : 
+    vs = analyzer.polarity_scores(quote)['compound']
+    if (vs >=0.05) :
+        return('positive')
+    if (vs <= - 0.05) :
+        return('negative') 
+    else : return('neutral')  
+    
+    quotes['sentiment'] = quotes['quotation'].apply(sentiment)  
+
   return None
